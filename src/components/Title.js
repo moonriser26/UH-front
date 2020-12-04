5e3f3c1e1207d5e1d0516a96f97ba062ea274628
alexandr <terekhovs@inbox.ru>
2020-12-04 15:15:38 +0300

    Test function getText()

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import picture from '../images/picture.jpg'
 
+
 function Title() {
+
+
+      const [text,setText] = useState('err')
+      const uerr = "http://jsonplaceholder.typicode.com/todos?_limit=5"
+      const URL = 'http://unix-history.org:4578/api/os/list/'
+
+       /* useEffect(() =>  { 
+            fetch(uerr)
+            .then(respons => respons.json())
+            .then(result => {
+                console.log(result[0]["title"])
+                //setText(result[0]["title"])
+            })
+        },[])*/
+
+        function getText(url) {
+            const headers = {
+                'Content-Type' : 'application/json',
+                'Access-Control-Allow-Origin' : 'http://unix-history.org:4578/api/os/list/',
+                'Access-Control-Allow-Credentials' : true,
+                'Access-Control-Allow-Methods' : 'GET,POST',
+            }
+
+            return fetch(url, {
+                method: 'GET',
+                mode: 'no-cors',
+                headers: headers,
+            }).then(response => response.json())
+            .then(res => {
+                setText(res[0]["html_text"])
+            })
+        }
+
     return(
         <div className ='block-title'>
-            <p className='title'>Title</p>       
+            <p className='title'>Немного о UNIX</p>       
             <div>
                 <img src={picture} className = 'title-img' alt='unix'></img>
                 <p className='subtitle-2'>Title</p>
                 <br/>
-                <p className='text-2'>
-                Дистинкция дискредитирует непредвиденный интеллект, учитывая опасность,
-                которую представляли собой писания Дюринга для не окрепшего еще немецкого
-                рабочего движения.
+                <p className="text-2" onChange={getText(URL)}>
+                   {text}
                 </p>
             </div>
             <p className='text-3'>
@@ -21,7 +53,7 @@ function Title() {
             рабочего движения.
             </p>
             <footer className='title-footer'>
-                <p>
+                <p >
                 © Unix History
                 </p>
             </footer>
