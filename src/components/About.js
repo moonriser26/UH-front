eb96a8adea1b5db9855a669f15aef70b83bd0176
alexandr <terekhovs@inbox.ru>
2020-12-04 23:15:34 +0300

    Change info OS

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,32 @@
 import React, {useEffect,useState} from 'react'
 
+
 function About() {
 
     const [infoLinux,setInfoLinux] = useState('')
     const [infoSolaris,setInfoSolaris] = useState('')
-    const [infoOpenBDS,setInfoOpenBDS] = useState('')
-      
-      const URL = 'http://unix-history.org:4578/api/os/list/'
-
-        useEffect(() =>  { 
-            fetch(URL)
-            .then(respons => respons.json())
-            .then(result => {
-                setInfoSolaris(result[1]["html_text"])
-                setInfoLinux(result[2]["html_text"])
-                setInfoOpenBDS(result[3]["html_text"])
-            })
-        },[])
+    const [infoOpenBSD,setInfoOpenBSD] = useState('')
+    const [infoFreeBSD, setInfoFreeBSD] = useState('')
+    const URL = 'http://unix-history.org:4578/api/os/list/'
+    
+    useEffect(() =>  { 
+        fetch(URL)
+        .then(respons => respons.json())
+        .then(result => {
+            setInfoSolaris(result[1]["html_text"])
+            setInfoLinux(result[2]["html_text"])
+            setInfoOpenBSD(result[3]["html_text"])
+            setInfoFreeBSD(result[4]["html_text"])
+        })
+    },[])
 
     return(
+
         <div className ='block-about'> 
             <p className='title'>С ЧЕГО БЫ НАЧАТЬ?</p>
             <p className='subtitle'>Семейство Unix</p>
             <p className='text'>
-            <h4>Solaris</h4>
+                <h4>Solaris</h4>
                 {infoSolaris}
             </p>
             <p className='text'>
@@ -32,17 +35,17 @@ function About() {
             </p>
             <p className='text'>
             <h4>OpenBSD</h4>
-                {infoOpenBDS}
+                {infoOpenBSD}
             </p>
             <p className='text'>
-                <h4>Title</h4>
-                Дистинкция дискредитирует непредвиденный интеллект, учитывая опасность,
-                которую представляли собой писания Дюринга для не окрепшего еще немецкого
-                рабочего движения.
-            </p>
-            
-        </div>
-    );
+                <h4>FreeBSD</h4>
+                    {infoFreeBSD}
+                </p>
+                
+            </div>
+
+    )
 }
+
 
 export default About
