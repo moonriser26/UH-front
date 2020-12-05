0e423566276a69642562f584e1a6d29b893cbc82
verdgil <me@verdgil.org>
2020-12-05 14:46:35 +0300

    add ssh form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Title from './components/Title'
+import SSH from "./components/ssh";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <About/>
       <Title/>
+      <SSH/>
     </div>
   );
 }
---
+++ src/components/ssh.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import picture from '../images/picture.jpg'
+import {url_static, url_socket} from '../settings'
+
+function SSH() {
+
+
+    const [text, setText] = useState('')
+
+    const URL = url_static + '/api/os/list/'
+
+    useEffect(() => {
+        fetch(URL)
+            .then(respons => respons.json())
+            .then(result => {
+                //console.log(result[0]["html_text"])
+                setText(result[0]["html_text"])
+            })
+    }, [])
+
+    document.onload = function () {
+        const socket = new WebSocket(url_socket + '/api/os/5/ssh')
+        // socket.onmessage = function (ev)
+        // отправить сообщение из формы publish
+        document.forms.publish.onsubmit = function () {
+            var outgoingMessage = this.message.value;
+
+            socket.send(outgoingMessage);
+            return false;
+        };
+
+// обработчик входящих сообщений
+        socket.onmessage = function (event) {
+            var incomingMessage = event.data;
+            showMessage(incomingMessage);
+        };
+
+// показать сообщение в div#subscribe
+        function showMessage(message) {
+            var messageElem = document.createElement('div');
+            messageElem.appendChild(document.createTextNode(message));
+            document.getElementById('subscribe').appendChild(messageElem);
+        }
+    }
+
+    return (
+        <div>
+            <form name="publish">
+                <div><input type="text" name="message"/></div>
+                <div><input type="submit" value="Отправить"/></div>
+            </form>
+            <div id="subscribe"></div>
+        </div>
+    );
+}
+
+export default SSH
