7f4fa851726916ee6b1997caf7e95b802b922001
alexandr <terekhovs@inbox.ru>
2021-05-16 23:39:01 +0300

    merge

--- src/Vers2/ssh.js
+++ src/Vers2/ssh.js
@@ -1,8 +1,4 @@
 import React, { useState, useEffect } from 'react'
-<<<<<<< HEAD
-=======
-import picture from '../images/picture.jpg'
->>>>>>> 6fd9b5708c9d6d91dd1330e384e40e6e12131882
 import {url_static, url_socket} from '../settings'
 import './OS.css'
 import './ssh.css'
@@ -11,42 +7,9 @@ import {sshMess} from './sshMess'
 
 function SSH(props) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD:src/components/ssh.js
-
-    const [text, setText] = useState('')
-
-<<<<<<< HEAD
-    const URL = url_static + '/api/os/list/'
-=======
-    const URL = url_static + 'api/os/list/'
->>>>>>> 6fd9b5708c9d6d91dd1330e384e40e6e12131882
-
-    useEffect(() => {
-        fetch(URL)
-            .then(respons => respons.json())
-            .then(result => {
-                //console.log(result[0]["html_text"])
-                setText(result[0]["html_text"])
-            })
-    }, [])
-
-<<<<<<< HEAD
-   /* document.addEventListener( "DOMContentLoaded",function () {
-        const socket = new WebSocket(url_socket + '/api/os/5/ssh')
-=======
-    document.addEventListener( "DOMContentLoaded",function () {
-        const socket = new WebSocket(url_socket + 'api/os/5/ssh')
->>>>>>> 6fd9b5708c9d6d91dd1330e384e40e6e12131882
-=======
-    /*document.addEventListener( "DOMContentLoaded",function () {
-        const socket = new WebSocket(url_socket + `/api/os/${props.id}/ssh`)
->>>>>>> dev2:src/Vers2/ssh.js
-=======
     //document.addEventListener( "DOMContentLoaded",
     setTimeout(function () {
         const socket = new WebSocket(url_socket + `api/os/${props.id}/ssh`);
->>>>>>> develop
         // socket.onmessage = function (ev)
         // отправить сообщение из формы publish
         socket.onopen = function(e) {
@@ -79,20 +42,9 @@ function SSH(props) {
         if (!props.active) {
             socket.close()
         }
-<<<<<<< HEAD:src/components/ssh.js
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
->>>>>>> dev2:src/Vers2/ssh.js
-    })*/
-=======
-    })
->>>>>>> 6fd9b5708c9d6d91dd1330e384e40e6e12131882
-=======
         
     }, 10);
->>>>>>> develop
 
     function handleKeyPress(e){
         var key=e.keyCode || e.which;
@@ -103,30 +55,13 @@ function SSH(props) {
        }
 
     return (
-<<<<<<< HEAD:src/components/ssh.js
-        <div>
-            <form name="publish">
-<<<<<<< HEAD
-                <div><input type="text" className="message"/></div>
-=======
-                <div><input type="text" name="message"/></div>
->>>>>>> 6fd9b5708c9d6d91dd1330e384e40e6e12131882
-                <div><input type="button" value="Отправить" id="button_send"/></div>
-            </form>
-            <div id="subscribe"></div>
-=======
         
         <div className='ssh-window'>          
             <div className='input-form message'>
                 <input type="text" placeholder='Ваш запрос (например: "uname -a")' id="ssh_command" onKeyPress={e => handleKeyPress(e)}/>
             </div>
             <div><input className='ssh-btn' type="submit" value="Отправить" id="button_send" /></div>          
-<<<<<<< HEAD
-            <div id="subscribe" className='ssh_mess'>Messages:</div>
->>>>>>> dev2:src/Vers2/ssh.js
-=======
             <div id="subscribe" className='ssh_mess'></div>
->>>>>>> develop
         </div>
     );
 }
